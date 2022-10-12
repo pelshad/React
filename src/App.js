b330a254c3c6bdcde9872c5bd2612bc2713fedcf
pelshad <akuaru@gmail.com>
2022-10-12 11:14:13 +0900

    이벤트 핸들러 클래스형

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React,{ useState } from 'react';
 // import VarStyle from './components/VarStyle'; //Test.jsx파일 import
 // import Event from './components/Event';
 // import Clock from './components/Clock';
-import Props from './components/Props';
+// import Props from './components/Props';
+import EventHandlingC from './components/EventHandlingC';
+
+// rcs 함수형으로 생성, rcc 클래스형으로 생성
 
 //함수형
  function App(){
@@ -18,7 +21,8 @@ import Props from './components/Props';
         <h1>Hello World!</h1>
         <button onClick={() => setCount(count + 1)}>{count}</button>
         <button onClick={textPrint}>값 출력</button> {/* textPrint함수 호출 */}
-        <Props name="React" stateSpec="high"/>; {/*Mycomponent에 name="React"를 보냄*/}
+        {/* <Props name="React" stateSpec="high"/>; {Mycomponent에 name="React"를 보냄} */}
+        <EventHandlingC />
       </div>
     );
 }
