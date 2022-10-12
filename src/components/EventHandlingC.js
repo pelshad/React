dd7487781f8875e169bbe0928f81f92509c39627
pelshad <akuaru@gmail.com>
2022-10-12 13:09:22 +0900

    클래스 이벤트 핸들러 끝

--- src/components/EventHandlingC.js
+++ src/components/EventHandlingC.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 class EventHandlingC extends Component {
 
     state = {
-        message: '',
+        username:'', //input의 username : username의 value로 연결
+        message: ''  //input의 message
       };
 
     //메소드 호출 ver.1(생성자 바인딩)
@@ -28,28 +29,47 @@ class EventHandlingC extends Component {
     //   }
     
     //메소드 호출 ver.2(화살표 함수)
-    handleChange = (e) => {
+    handleChange = e => {
         this.setState({
-            message : e.target.value
+            [e.target.name] : e.target.value,
+            //[e.target.name] 객체 안의 각 필드값을 가져온다.
         });
-    }
+    };
+
     handleClick = () => {
-        alert(this.state.message);
+        alert(this.state.username + ': ' + this.state.message);
         this.setState({
-            message : ''
+            message : '',
+            username: ''
         });
     }
 
+    handleKeyPress = e =>{
+        if(e.key === 'Enter'){
+            this.handleClick();
+        }
+    };
+
+    //핸들러 함수 
+    //onClick 클릭이벤트, onKeyPress 키를 눌렀을때, onChange 변화가 생겼을때
     render() {
         return (
             <div>
                 <h1>Event Practice</h1>
                 <input
                 type="text"
+                name="username"
+                placeholder="type something"
+                value={this.state.username}
+                onChange={this.handleChange}
+                />
+                <input
+                type="text"
                 name="message"
                 placeholder="type something"
                 value={this.state.message}
                 onChange={this.handleChange}
+                onKeyPress={this.handleKeyPress}
                 />
                 <button onClick={this.handleClick}>Submit</button>
             </div>
